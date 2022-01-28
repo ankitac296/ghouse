77dccd4794b4a7bd8f5876e604614d25b83f0683
ankitac296 <98546198+ankitac296@users.noreply.github.com>
2022-01-28 23:11:40 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,64 @@
+  <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "greenhouse";
+
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully";
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
